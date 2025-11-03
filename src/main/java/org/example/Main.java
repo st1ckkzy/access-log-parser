7650b5e0960725c0bcc81fd227c37cfc42523a95
Dmitry Doronin <git.user.topfrost@gmail.com>
2025-11-03 20:54:32 +0300

    changed main logic to make a simple calculator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Введите первое число:");
+        int x = scanner.nextInt();
+
+        System.out.println("Введите второе число:");
+        int y = scanner.nextInt();
+
+        int add = x + y;
+        int subtr = x - y;
+        int multipl = x * y;
+        double div = (double) x / y;
+
+        System.out.println(x + " + " + y + " = " + add);
+        System.out.println(x + " - " + y + " = " + subtr);
+        System.out.println(x + " * " + y + " = " + multipl);
+        System.out.println(x + " / " + y + " = " + div);
     }
 }
