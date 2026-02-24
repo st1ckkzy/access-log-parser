8ef524826098be532e97dad29de55aee8fbe67c6
Dmitry Doronin <git.user.topfrost@gmail.com>
2026-02-24 18:50:41 +0300

    9.3.1 implemented exception handler for log analyzer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
+
+class LineTooLongException extends RuntimeException {
+    public LineTooLongException(String message) {
+        super(message);
+    }
+}
 
 public class Main {
 
@@ -10,7 +18,6 @@ public class Main {
 
         while (true) {
             System.out.print("Введите путь к файлу: ");
-
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
             boolean fileExists = file.exists();
@@ -31,6 +38,39 @@ public class Main {
             System.out.println("Путь указан верно");
             System.out.println("Это файл номер " + fileExistsCounter);
             System.out.println();
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                FileReader fileReader = new FileReader(path);
+                String line;
+                int totalLines = 0;
+                int minLength = Integer.MAX_VALUE;
+                int maxLength = 0;
+
+                while ((line = reader.readLine()) != null) {
+                    int length = line.length();
+
+                    if (length > 1024) {
+                        throw new LineTooLongException("Строка #" + (totalLines + 1) + " превышает 1024 символа. Длина: " + length);
+                    }
+
+                    totalLines++;
+                    if (length < minLength) minLength = length;
+                    if (length > maxLength) maxLength = length;
+                }
+
+                reader.close();
+                fileReader.close();
+
+                System.out.println("Количество строк: " + totalLines);
+                System.out.println("Минимальная длина: " + (minLength == Integer.MAX_VALUE ? 0 : minLength));
+                System.out.println("Максимальная длина: " + maxLength);
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                System.exit(1);
+            }
         }
     }
 }
+
+
